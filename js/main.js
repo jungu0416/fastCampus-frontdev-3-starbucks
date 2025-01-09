4e04f63df0166eab6f289f4c6393b99f4c34c11a
jungu0416 <jungu0416@naver.com>
2025-01-09 15:36:39 +0900

    badge styling

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ searchInputEl.addEventListener('blur', function() {
   searchEL.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .4, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .4, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+},300));
